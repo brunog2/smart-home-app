e4eaed618fe6976c8245d9e80a5eac2810375487
Bruno Gomes <brunofilho2k02@gmail.com>
2021-03-23 12:56:00 -0300

    Algumas alteraçoes na interface 1

--- App.js
+++ App.js
@@ -1,23 +1,79 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, StatusBar, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TouchableOpacity, FlatList } from 'react-native';
 import AppLoading from 'expo-app-loading';
+import { LinearGradient } from 'expo-linear-gradient';
 import { Ionicons } from '@expo/vector-icons';
 import { useFonts, Lato_700Bold, Lato_400Regular, Lato_300Light } from '@expo-google-fonts/lato';
 export default function App() {
-  const [sitSensor, setSitSensor] = useState(false);
-
-  
+  const [sensores, setSensores] = useState([
+      {
+        'id': 'sala',
+        'name': 'Luz da sala',
+        'sit': 0,
+        },
+        {
+          'id': 'piscina',
+          'name': 'Luz da piscina',
+          'sit': 0,
+        },
+        {
+          'id': 'alarme',
+          'name': 'Alarme',
+          'sit': 0,
+        }
+    ]);
+    
+  const colors = {
+    'icon': { 'disabled': '#7b6492', 'enabled': '#f5c242' },
+    'switchIcon': { 'disabled': '#756788', 'enabled': '#fff' },
+    'switchBg': { 'disabled': 'transparent', 'enabled': '#f88d3d' },
+    'switchBorder': { 'disabled': '#756788', 'enabled': 'transparent' },
+    'statusDesc': { 'disabled': '#756788', 'enabled': '#14be8f' },
+  };
 
   let [fontsLoaded] = useFonts({
+    'BasisGrotesqueProRegular': require('./assets/fonts/BasisGrotesquePro-Regular.ttf'),
+    'BasisGrotesqueProBold': require('./assets/fonts/BasisGrotesquePro-Bold.ttf'),
+    'BasisGrotesqueProLight': require('./assets/fonts/BasisGrotesquePro-Light.ttf'),
     Lato_700Bold, 
     Lato_400Regular,
     Lato_300Light
   });
 
+  useEffect(() => {
+    console.log(sensores)
+  }, [sensores])
+
+  const handleSuperButtonPress = () => {
+    var newSensores = [...sensores];
+
+    for (var sensor in newSensores) {
+      newSensores[sensor].sit = 0;
+    };
+
+    setSensores(newSensores);
+  };
+
+  const handleSwitchPress = (id) => {
+    var newSensores = [...sensores];
+
+    console.log(newSensores, id);
+
+    for (var sensor in newSensores) {
+      console.log(sensor, newSensores[sensor].id)
+      if (newSensores[sensor].id == id)
+        newSensores[sensor].sit = newSensores[sensor].sit == 0 ? 1 : 0;
+    }    
+
+    console.log(newSensores);
+
+    setSensores(newSensores);
+  }
+
   if (!fontsLoaded) {
     return <AppLoading />;
   }
-
+  
   else {
       return (
       <View style={styles.mainContainer}>      
@@ -46,25 +102,43 @@ export default function App() {
 
         <View style={[styles.container, styles.bottomContainer]}>
           <View style={styles.cardsContainer}>
-            <View style={styles.sensorCard}>
-              <View style={styles.sensorCardIconsContainer}>
-                <Ionicons name={sitSensor == false ? "bulb-outline" : "bulb" } size={24} color={sitSensor == false ? "#000" : "#f5c242"} />
+            <FlatList
+              data={sensores}
+              keyExtractor={item => item.id}
+              numColumns={2}
+              renderItem={({ item }) => {
+                return (
+                  <View style={styles.sensorCard}>
+                    <View style={styles.sensorCardIconsContainer}>
+                      <Ionicons name={item.sit == 0 ? "bulb-outline" : "bulb" } size={24} color={item.sit == 0 ? colors["icon"].disabled : colors["icon"].enabled} />
 
-                <TouchableOpacity style={[styles.sensorStatusIcon, {borderWidth: sitSensor == false ? 1 : 0, borderColor: sitSensor == false ? "#dadada" : "#f88d3d", backgroundColor: sitSensor == false ? "#fff" : "#f88d3d"}]} onPress={() => setSitSensor(!sitSensor)}>
-                  <Ionicons name="power-outline" size={18} color={sitSensor == false ? "#dadada" : "#fff"} />
-                </TouchableOpacity>
-              </View>
+                      <TouchableOpacity 
+                      style={[
+                        styles.sensorStatusIcon, 
+                        {
+                          borderWidth: item.sit == 0 ? 1 : 0, 
+                          borderColor: item.sit == 0 ? colors['switchBorder'].disabled : colors['switchBorder'].enabled, 
+                          backgroundColor: item.sit == 0 ? colors['switchBg'].disabled : colors['switchBg'].enabled
+                          }
+                        ]} 
+                        onPress={() => handleSwitchPress(item.id)}>                        
+                        <Ionicons name="power-outline" size={18} color={ item.sit == 0 ? colors["switchIcon"].disabled : colors["switchIcon"].enabled } />
+                      </TouchableOpacity>
+                    </View>
 
-              <View style={styles.sensorDescriptionContainer}> 
-                <Text style={styles.sensorTitle}>
-                  Luz da sala
-                </Text>          
+                    <View style={styles.sensorDescriptionContainer}> 
+                      <Text style={styles.sensorTitle}>
+                        {item.name}
+                      </Text>          
 
-                <Text style={[styles.sensorStatus, {color: sitSensor == false ? "#dadada" : "#3eed61"}]}>
-                  {sitSensor == false ? "Off" : "On"}
-                </Text>
-              </View>
-            </View>
+                      <Text style={[styles.sensorStatus, {color: item.sit == 0 ? colors['statusDesc'].disabled : colors['statusDesc'].enabled }]}>
+                        {item.sit == 0 ? "Off" : "On"}
+                      </Text>
+                    </View>
+                  </View>  
+                )
+              }}
+              />
           </View>        
         </View>
       </View>
@@ -78,7 +152,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: "#49cd6a"
+    backgroundColor: '#e3e4e9'
   },
   container: {
     borderColor: "black",
@@ -88,11 +162,14 @@ const styles = StyleSheet.create({
     flex: 0.4,
     alignItems: 'center',
     justifyContent: 'space-evenly',
+    borderBottomStartRadius: 30,
+    borderBottomEndRadius: 30,
+    backgroundColor: "#5200ba"
   },
   title: {
     fontSize: 26,
     color: "#fff",
-    fontFamily: 'Lato_700Bold'
+    fontFamily: 'BasisGrotesqueProBold'
   },
   infoContainer: {
     flexDirection: 'row',
@@ -103,12 +180,12 @@ const styles = StyleSheet.create({
   temperature: {
     fontSize: 18,
     color: '#fff',
-    fontFamily: "Lato_400Regular"
+    fontFamily: "BasisGrotesqueProRegular"
   },
   date: {
     fontSize: 18,
     color: '#fff',
-    fontFamily: "Lato_400Regular"
+    fontFamily: "BasisGrotesqueProRegular"
   },
   shutdownAllContainer: {
     width: 100,
@@ -136,24 +213,27 @@ const styles = StyleSheet.create({
   },
   bottomContainer: {
     flex: 0.6,
-    borderTopEndRadius: 35,
-    borderTopStartRadius: 35,
-    backgroundColor: "#f5f6f7",
+    
+    backgroundColor: "#e3e4e9",
     alignItems: "center",
     justifyContent: 'center'
   },
   cardsContainer: {
     width: "90%",
     borderColor: "black",
-    flex: 0.9,    
+    flex: 0.9,
+    maxHeight: 380,
+    
   },
   sensorCard: {
-    width: 135,
+    flexGrow: 1,
+    flexBasis: 0,
+    margin: 6,
     height: 135,
     borderRadius: 15,
     justifyContent: 'space-between',
     padding: 15,
-    backgroundColor: "#fff",
+    backgroundColor: "#f2f2f2",
     shadowColor: "#f0f0f0",
     shadowOffset: {
       width: 0,
@@ -162,7 +242,6 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.25,
     shadowRadius: 3.84,
     elevation: 5,
-    
   },
   sensorCardIconsContainer: {
     flexDirection: 'row',
@@ -184,11 +263,13 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: '500',
     marginBottom: 5,
+    color: '#4e4e4e',
     fontFamily: 'Lato_300Light',
   },
   sensorStatus: {
     fontSize: 16,
-    color: "#dadada",
+    color: "#756788",
     fontFamily: 'Lato_300Light',
   }
 });
+
